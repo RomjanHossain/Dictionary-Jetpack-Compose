847b71511da74f923f342ad28cfe9867f213e27c
RomjanHossain <romjan1412@proton.me>
2025-12-18 11:45:53 +0600

    dictionary repo + di

--- app/src/main/java/com/capx/dictionary/di/repository/DictionaryRepoDi.kt
+++ app/src/main/java/com/capx/dictionary/di/repository/DictionaryRepoDi.kt
@@ -1,4 +1,19 @@
 package com.capx.dictionary.di.repository
 
-class DatabaseRepoDi {
+import com.capx.dictionary.data.dao.DictionaryDao
+import com.capx.dictionary.domain.repository.DictionaryRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DictionaryRepoDi {
+    @Provides
+    @Singleton
+    fun provideDictionaryRepository(dao: DictionaryDao): DictionaryRepository {
+        return DictionaryRepository(dao)
+    }
 }
--- app/src/main/java/com/capx/dictionary/domain/repository/DictionaryRepository.kt
+++ app/src/main/java/com/capx/dictionary/domain/repository/DictionaryRepository.kt
@@ -1,4 +1,60 @@
 package com.capx.dictionary.domain.repository
 
-class DictionaryRepository {
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.capx.dictionary.data.dao.DictionaryDao
+import com.capx.dictionary.data.entity.DictionaryFts
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class DictionaryRepository @Inject constructor(
+    private val database: DictionaryDao
+) {
+
+    fun getB2b(): Flow<PagingData<DictionaryFts>> {
+        return Pager(
+            PagingConfig(
+                pageSize = 30,
+                enablePlaceholders = false
+            ), pagingSourceFactory = {
+                database.getAllb2b()
+            }
+        ).flow
+    }
+
+    fun getB2e(): Flow<PagingData<DictionaryFts>> {
+        return Pager(
+            PagingConfig(
+                pageSize = 30,
+                enablePlaceholders = false
+            ), pagingSourceFactory = {
+                database.getAllb2e()
+            }
+        ).flow
+    }
+
+    fun getE2b(): Flow<PagingData<DictionaryFts>> {
+        return Pager(
+            PagingConfig(
+                pageSize = 30,
+                enablePlaceholders = false
+            ), pagingSourceFactory = {
+                database.getAlle2b()
+            }
+        ).flow
+    }
+
+    fun getE2e(): Flow<PagingData<DictionaryFts>> {
+        return Pager(
+            PagingConfig(
+                pageSize = 30,
+                enablePlaceholders = false
+            ), pagingSourceFactory = {
+                database.getAlle2e()
+            }
+        ).flow
+    }
+
 }

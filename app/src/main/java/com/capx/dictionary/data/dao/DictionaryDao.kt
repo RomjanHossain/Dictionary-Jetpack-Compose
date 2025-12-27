95f42843c417afeb9dd380b8985444af7cda4b67
RomjanHossain <romjan1412@proton.me>
2025-12-28 00:02:17 +0600

    get letters

--- app/src/main/java/com/capx/dictionary/data/dao/DictionaryDao.kt
+++ app/src/main/java/com/capx/dictionary/data/dao/DictionaryDao.kt
@@ -19,9 +19,16 @@ interface DictionaryDao {
     @Query("SELECT distinct title FROM bangladic_fts where lower(title) like lower(:query)||'%'")
     fun searchFts(query: String): PagingSource<Int, DictionaryFts>
 
-    @Query("SELECT distinct title FROM bangladic_fts where original_file=='b2b' or original_file=='b2e'")
-    fun getAllbanglaTitles(): PagingSource<Int, DictionaryFts>
+    @Query("SELECT distinct title FROM bangladic_fts where original_file=='b2b' or original_file=='b2e' and title like :letter or title like upper(:letter)")
+    fun getAllbanglaTitles(letter: String): PagingSource<Int, DictionaryFts>
 
-    @Query("SELECT distinct title FROM bangladic_fts where original_file=='e2e' or original_file=='e2b'")
-    fun getAllenglishTitles(): PagingSource<Int, DictionaryFts>
+    @Query("SELECT distinct UPPER(substr(title,1,1)) as title FROM bangladic_fts where original_file=='b2b' or original_file=='b2e'")
+    suspend fun getAllbanglaLetters(): List<DictionaryFts>
+
+    @Query("SELECT distinct title FROM bangladic_fts where original_file=='e2e' or original_file=='e2b' and title like lower(:letter) or title like upper(:letter)")
+    fun getAllenglishTitles(letter: String): PagingSource<Int, DictionaryFts>
+
+    //    SELECT DISTINCT substr(title,1,1) as f from bangladic where original_file=="b2e" or original_file=="b2b";
+    @Query("SELECT distinct Upper(substr(title,1,1)) as title FROM bangladic_fts where original_file=='e2e' or original_file=='e2b'")
+    suspend fun getAllenglishLetters(): List<DictionaryFts>
 }
